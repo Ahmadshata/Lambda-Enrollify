f7f3d7233543d2fbee065874946d5ef0ddacb649
Ahmadshata <ahmadesmailshata@gmail.com>
2023-09-04 19:27:12 +0300

    Removed the old invoke-URL form the outputs.

--- output.tf
+++ output.tf
@@ -1,6 +1,3 @@
-output "invoke-url" {
-  value = module.enrollment-api.invoke-url
-}
 
 output "custom-domain-invoke-url" {
   value = "https://${module.enrollment-api.custom-domain-name}/${module.enrollment-api.resource-name}"
