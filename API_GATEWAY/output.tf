6f177fd007d2c23ac5351d9b02caf38d795cf177
Ahmadshata <ahmadesmailshata@gmail.com>
2023-09-04 19:07:45 +0300

    The API invoke-URL is an output now.

--- API_GATEWAY/output.tf
+++ API_GATEWAY/output.tf
@@ -5,3 +5,11 @@ output "api-execution-arn" {
 output "invoke-url" {
   value = aws_api_gateway_deployment.api-deployment.invoke_url
 }
+
+output "custom-domain-name" {
+  value = aws_api_gateway_domain_name.custom-domain.domain_name
+}
+
+output "resource-name" {
+  value = aws_api_gateway_resource.enrollment-resource.path_part
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "invoke-url" {
   value = module.enrollment-api.invoke-url
 }
+
+output "custom-domain-invoke-url" {
+  value = "${module.enrollment-api.custom-domain-name}/${module.enrollment-api.resource-name}"
+}
